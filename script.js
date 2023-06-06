8f1a47da84872fa85ae38d0e9a9f38e2cd54015e
DragonBlessed <christianvasil72@gmail.com>
2023-06-06 02:41:40 -0700

    able to render popup with content fixed icon mistake

---
+++ script.js
@@ -0,0 +1,11 @@
+var enabled = false;
+// research
+const disenable = document.querySelectorAll('enablingbutton');
+enablingbutton.addEventListener('click', () => {
+    
+})
+
+
+function removeVideo() {
+    
+}
