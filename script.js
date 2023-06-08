2b3edc45b075e74dbda847fbbed2a030ebfbf538
DragonBlessed <christianvasil72@gmail.com>
2023-06-08 02:08:10 -0700

    functions in progress

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-var enabled = false;
-// research
-const disenable = document.querySelectorAll('button');
-button.addEventListener('click', () => {
-    
-})
+var enabled = localStorage.getItem('videoEnabled') === 'true' || false;
+let player;
 
-
-function removeVideo() {
-    const playerElement = document.getElementById("player");
-    playerElement.style.display = "none";
+function disenableVideo() {
+  if (enabled) {
+    player.unMute();
+    const videoElement = player.getIframe();
+    videoElement.style.visibility = 'visible';
+  } else {
+    player.mute();
+    const videoElement = player.getIframe();
+    videoElement.style.visibility = 'hidden';
+  }
 }
+
+function onPlayerReady(event) {
+  player = event.target;
+  disenableVideo();
+
+  const button = document.getElementById('enablingbutton');
+
+  button.addEventListener('click', () => {
+    enabled = !enabled;
+    disenableVideo();
+    localStorage.setItem('videoEnabled', enabled.toString());
+  });
+}
+
+function loadYouTubeIframeAPI() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  
+    tag.onload = () => {
+      tag.remove();
+    };
+  }
+  
+  loadYouTubeIframeAPI();
+
+
 
 
 // Miniplayer = miniplayer style-scope ytd-miniplayer
